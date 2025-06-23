3b6839c868a5bb287365379748126767f3e4b464
Shashwat252003 <shashwat.bajpai@pandu.ai>
2025-06-24 01:30:10 +0530

    made the synatx fix for entrypoint.sh file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,4 @@
-# #! /bin/sh
-
-# # Wait for the database to be up
-# until nc -z db 5432; do
-#   echo "Waiting for db to be up..."
-#   sleep 2
-# done
-
-# # Apply migrations
-# npx prisma migrate deploy
-
-# # Start your server
-# npm run dev
-
-
-
-
 #!/bin/sh
-
 # Optional: wait for database host to be reachable (for safety)
 # until pg_isready -h "$DB_HOST" -p 5432 -U "$DB_USER"; do
 #   echo "Waiting for PostgreSQL..."
